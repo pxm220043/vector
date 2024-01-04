9e7213274be738c7c891a9eb18faee20a658797e
pxm220043 <145625808+pxm220043@users.noreply.github.com>
2024-01-04 02:23:23 +0000

    stash apply

--- .ccls-cache/@home@runner@inpud-validation/main.cpp
+++ .ccls-cache/@home@runner@inpud-validation/main.cpp
@@ -12,3 +12,12 @@ vector<double> prices {9.99, 12.99, 4.50, 0.99, 2.99, 6.25, 8.75};
   
   return 0;
 }
+// you should see this in repl
+// you should see this also after using git pull
+// you should be able to see this as well
+//line 2
+
+
+
+//I WILL USE GIT FETCH
+//I WILL USE GIT FETCH2
